b0eacc56d7bbe24d08afc1601d648d3517771c5f
moshaban <moshaban@SH-MOSTAFA-SHAB>
2022-01-29 13:23:42 +0200

    login alert

--- src/views/pages/authentication/Login.js
+++ src/views/pages/authentication/Login.js
@@ -63,6 +63,7 @@ const Login = props => {
       useJwt
         .login({ username, password })
         .then(res => {
+          console.log(res)
           const data = { ...res.data.data.userData, accessToken: res.data.data.accessToken, refreshToken: res.data.data.refreshToken, expiration: res.data.data.expiration }
           dispatch(handleLogin(data))
           ability.update(res.data.data.userData.ability)
