f2c2860eece273d9df04ccd0a614133e5f85b849
mfahmed <mohammed.fouad208@gmail.com>
2022-02-20 20:51:22 +0200

    Classification

--- src/views/indicator/classification.js
+++ src/views/indicator/classification.js
@@ -14,16 +14,11 @@ const classification = (props) => {
 
     const [ClassificationValues, setClassificationValues] = useState([])
     const [ClassificationId, setClassificationId] = useState()
-    const [ParentId, setParentId] = useState()
 
     const getClassificationValues = async (classificationId, parentId) => {
-        console.log(ClassificationId)
-        console.log(parentId)
-
         if (classificationId) {
             parentId = parentId ? parentId : 0
             const Url = `/ClassificationValue/GetClassificationValuesWithIndicatorsCount/${classificationId}/${parentId}`
-            console.log(Url)
             await axios.get(Url)
             .then(response => {
                 console.log(response.data.data)
@@ -34,27 +29,16 @@ const classification = (props) => {
                })
         }
     }
-    // console.log(props.match.params)
-    // console.log(ParentId)
-    // getClassificationValues(props.match.params.classificationId, props.match.params.parentId)
-
-    // if (props.match.params.classificationId) {
-    //     getClassificationValues(props.match.params.classificationId, props.match.params.parentId)
-    // }
-
-    // if (parseInt(props.match.params.classificationId) !== parseInt(ClassificationId) && parseInt(props.match.params.parentId) !== parseInt(ParentId)) {
-    //     getClassificationValues(props.match.params.classificationId, props.match.params.parentId)
-    //     setClassificationId(props.match.params.classificationId)
-    //     setParentId(props.match.params.parentId)
-    // } else if (parseInt(props.match.params.classificationId) !== parseInt(ClassificationId)) {
-    //     getClassificationValues(props.match.params.classificationId, 0)
-    //     setClassificationId(props.match.params.classificationId)
-    // }
     useEffect(() => {
         if (!ClassificationId && store.classifications.length > 0) {
             getClassificationValues(store.classifications[0].id)
         }
     }, [store.classifications])
+
+    useEffect(() => {
+        getClassificationValues(props.match.params.classificationId, props.match.params.parentId)
+    }, [props.match.params])
+    
   return (
       <>
       {<IndicatorHeader classificationId={props.match.params.classificationId} tabEnumValue={tabEnum.classification} /> }
