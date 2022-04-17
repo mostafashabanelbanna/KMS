ed501b3b627a8e6e4a41041efe468d17308a2bf7
mfahmed <mohammed.fouad208@gmail.com>
2022-04-17 14:41:17 +0200

    Changes

--- src/views/apps/homepage/index.js
+++ src/views/apps/homepage/index.js
@@ -1,14 +1,18 @@
 
 // ** Styles
 import '@styles/react/apps/app-email.scss'
-
+import HomeCard from './homePageCard'
+import Slider from './slider'
 
 const Homepage = () => {
  
   return (
-    <div className="flex-grow-1">
+    <div style={{overflowY: "auto"}}>
       <h4>الصفحة الرئيسية</h4>
-      
+      <HomeCard title={"إضيف حديثا"} addedLatelyComp={true}/>
+      <HomeCard title={"المفضلة"}/>
+      <Slider title={"لوحات معلوماتية"}  dashboard={true}/>
+      <Slider title={"مصادر مميزة على الإنترنت"}/>
     </div>
   )
 }
