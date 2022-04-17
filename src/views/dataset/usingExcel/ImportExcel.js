d30f6e75b43d324ea011c40cede380c6acff36ef
mfahmed <mohammed.fouad208@gmail.com>
2022-04-18 00:01:35 +0200

    Import Data

--- src/views/dataset/usingExcel/ImportExcel.js
+++ src/views/dataset/usingExcel/ImportExcel.js
@@ -1,27 +1,58 @@
 
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Card, CardHeader, CardTitle, CardBody, FormGroup, Label, Button, CustomInput, Row, Col } from 'reactstrap'
+import { Card, CardHeader, CardTitle, CardBody, FormGroup, Label, Button, Input, CustomInput, Row, Col } from 'reactstrap'
 import { toast } from 'react-toastify'
 import { useIntl } from 'react-intl'
 
 import { importFile } from '../store/action'
+import Select from 'react-select'
+import { isObjEmpty, getSelected, selectThemeColors, convertToBoolean } from '@utils'
 
 import Toastr from '../../../containers/toastr/Toastr'
+import axios from '../../../axios'
 
 const ImportExcel = () => {
+
+  
   // ** Store Vars
   const dispatch = useDispatch()
   const store = useSelector(state => state.datasets)
 
   // state
   const [file, setFile] = useState()
+  const [dataQuality, setDataQuality] = useState([])
+  const [datasetAttachments, setDatasetAttachments] = useState()
+  const [description_A, setDescriptionA] = useState()
+  const [description_E, setDescriptionE] = useState()
+  const [dataQualityId, setDataQualityId] = useState()
 
+  const getDataQuality = async () => {
+    const response = await axios
+      .post(`/Lookups/GetLookupValues/`, {lookupName: "DataQuality"})
+      .catch((err) => console.log("Error", err)) //handle errors
+     
+      if (response && response.data) {
+          setDataQuality(response.data.data)
+      }
+  } 
   const handleFinleInput = (e) => {
       setFile(e.target.files)
   }
+  const handleDatasetAttachments = (e) => {
+    setDatasetAttachments(e.target.files)
+  }
+  const handleDescriptionAChange = (e) => {
+    setDescriptionA(e.target.value)
+  }
+  const handleDescriptionEChange = (e) => {
+    setDescriptionE(e.target.value)
+  }
+  const handleDataQualityChange = (e) => {
+    setDataQualityId(e.id)
+  }
   const handleSubmit = () => {
-    dispatch(importFile(file))
+    dispatch(importFile({file, description_A, description_E, dataQualityId, datasetAttachments}))
   }
 
   // Import localization files
@@ -36,6 +67,9 @@ const ImportExcel = () => {
 }
   // handle Errors
   
+  useEffect(() => {
+    getDataQuality()
+  }, [])
   useEffect(() => {
     const code = store.importResponse.statusCode
     if (code !== 0) {
@@ -53,7 +87,6 @@ const ImportExcel = () => {
     }
   }, [store.importResponse.statusCode])
 
-
     return (
       <>
         <Card>
@@ -62,8 +95,62 @@ const ImportExcel = () => {
             </CardHeader>
             <CardBody>
               <FormGroup>
+                <Label for='descriptionA'>
+                {intl.formatMessage({id: "Description"})}
+                </Label>
+                <Input
+                  name='descriptionA'
+                  id='descriptionA'
+                  type="textarea"
+                  defaultValue=''
+                  placeholder={intl.formatMessage({id: "Description"})}
+                  onChange={(e) => handleDescriptionAChange(e)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for='descriptionE'>
+                {intl.formatMessage({id: "descriptionE"})}
+                </Label>
+                <Input
+                  name='descriptionE'
+                  id='descriptionE'
+                  type="textarea"
+                  defaultValue=''
+                  placeholder={intl.formatMessage({id: "descriptionE"})}
+                  onChange={(e) => handleDescriptionEChange(e)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>
+                   جودة البيانات
+                </Label>
+                <Select
+                  placeholder="تحديد"
+                  isClearable={false}
+                  theme={selectThemeColors}
+                  name='dataQuality'
+                  id='dataQuality'
+                  options={dataQuality}
+                  getOptionLabel={(option) => option.name}
+                  getOptionValue={(option) => option.id}
+                  className='react-select'
+                  classNamePrefix='select'
+                  onChange={e => handleDataQualityChange(e) }
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>
+                   الملفات المرفقة
+                </Label>
+               <CustomInput onChange={(e) => handleDatasetAttachments(e)}  type='file' id='datasetAttachments' name='datasetAttachments' />
+             </FormGroup>
+              <FormGroup>
+                <Label>
+                  ملف البيانات
+                </Label>
                <CustomInput onChange={(e) => handleFinleInput(e)}  type='file' id='exampleCustomFileBrowser' name='customFile' />
              </FormGroup>
+
             </CardBody>
         </Card>
         <Col className="text-left d-flex justify-content-end" md={12} >
