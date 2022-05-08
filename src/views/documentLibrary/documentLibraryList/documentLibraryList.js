6a86d414878a144a6fccf249484ab27faa38e78c
mfahmed <mohammed.fouad208@gmail.com>
2022-05-08 09:35:06 +0200

    ss

---
+++ src/views/documentLibrary/documentLibraryList/documentLibraryList.js
@@ -0,0 +1,11 @@
+import PublishInformation from "./publishInfoSection"
+
+const DocumentLibraryList = () => {
+    return (
+        <div className="d-flex">
+            <PublishInformation/>
+        </div>
+    )
+}
+
+export default DocumentLibraryList
---
+++ src/views/documentLibrary/documentLibraryList/publishInfoSection.js
@@ -0,0 +1,13 @@
+const PublishInformation = () => {
+    return (
+        <div className="card d-flex flex-column">
+            <div className="d-flex">
+                <p>بيانات الإصدارة</p>
+                <p>X</p>
+            </div>
+            <hr className="col-12 bg-gray my-0 px-2" />
+        </div>
+    )
+}
+
+export default PublishInformation
